dd3ab30545d2fb86f9530f6f73100a7e3e9e41c1
Hoa Pham <hpham@StevesDoors.com>
2019-06-26 09:31:53 -0500

    Finish Razor Views section from PluralSight Course:

--- OdeToFood/OdeToFood/Services/InMemoryRestaurantData.cs
+++ OdeToFood/OdeToFood/Services/InMemoryRestaurantData.cs
@@ -5,35 +5,35 @@ using OdeToFood.Models;
 
 namespace OdeToFood.Services
 {
-    public class InMemoryRestaurantData : IRestaurantData
-    {
-        private List<Restaurant> _restaurant;
+    //public class InMemoryRestaurantData : IRestaurantData
+    //{
+    //    private List<Restaurant> _restaurant;
 
-        public IEnumerable<Restaurant> GetAll()
-        {
-            return _restaurant.OrderBy(r => r.Name);
-        }
+    //    public IEnumerable<Restaurant> GetAll()
+    //    {
+    //        return _restaurant.OrderBy(r => r.Name);
+    //    }
 
-        public Restaurant Get(int id)
-        {
-            return _restaurant.FirstOrDefault(r => r.Id == id);
-        }
+    //    public Restaurant Get(int id)
+    //    {
+    //        return _restaurant.FirstOrDefault(r => r.Id == id);
+    //    }
 
-        public InMemoryRestaurantData()
-        {
-            _restaurant = new List<Restaurant>
-            {
-                new Restaurant { Id = 1, Name = "Pho Hoa" },
-                new Restaurant { Id = 2, Name = "Pho Hoa 2" },
-                new Restaurant { Id = 3, Name = "Pho Hoa 3" }
-            };
-        }
+    //    public InMemoryRestaurantData()
+    //    {
+    //        _restaurant = new List<Restaurant>
+    //        {
+    //            new Restaurant { Id = 1, Name = "Pho Hoa" },
+    //            new Restaurant { Id = 2, Name = "Pho Hoa 2" },
+    //            new Restaurant { Id = 3, Name = "Pho Hoa 3" }
+    //        };
+    //    }
 
-        public Restaurant Add(Restaurant restaurant)
-        {
-            restaurant.Id = _restaurant.Max(r => r.Id) + 1;
-            _restaurant.Add(restaurant);
-            return restaurant;
-        }
-    }
+    //    public Restaurant Add(Restaurant restaurant)
+    //    {
+    //        restaurant.Id = _restaurant.Max(r => r.Id) + 1;
+    //        _restaurant.Add(restaurant);
+    //        return restaurant;
+    //    }
+    //}
 }
